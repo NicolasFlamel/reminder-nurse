52db45fd855f9f0825cabed0ce673839d18c6489
Amauri <hamauri@aol.com>
2025-06-30 15:12:00 -0700

    created codegen file

---
+++ server/codegen.ts
@@ -0,0 +1,13 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+import { typeDefs } from './src/schema';
+
+const config: CodegenConfig = {
+  schema: typeDefs,
+  generates: {
+    './src/types/schemaTypes.ts': {
+      plugins: ['typescript', 'typescript-resolvers'],
+    },
+  },
+};
+
+export default config;
