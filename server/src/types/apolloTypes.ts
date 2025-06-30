34a1911773f0ef19a164db1a4303eba83586fad9
Amauri <hamauri@aol.com>
2025-06-30 13:43:33 -0700

    created Apollo types

---
+++ server/src/types/apolloTypes.ts
@@ -0,0 +1,17 @@
+export interface MyContext {
+  token?: string;
+  user: { username: string; _id: string };
+}
+
+export type ResolverType = {
+  Query: QueryType;
+  Mutation: QueryType;
+};
+
+type QueryType = {
+  [key: string]: (
+    parent: unknown,
+    args: { [key: string]: unknown },
+    context: MyContext,
+  ) => any;
+};
