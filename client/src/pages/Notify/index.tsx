741d1d39f5c8507c347fa1d01b2f7f93ba5a6b23
Amauri <hamauri@aol.com>
2025-07-15 12:29:49 -0700

    updated Notify component
    component button when clicked now checks for permission and requests it if it doesn't have it

--- client/src/pages/Notify/index.tsx
+++ client/src/pages/Notify/index.tsx
@@ -2,8 +2,12 @@ import Notifications from 'utils/Notifications';
 import { Button, Container } from 'react-bootstrap';
 
 export const Notify = () => {
-  const testNotification = () =>
+  const testNotification = () => {
+    if (Notifications.permission !== 'granted')
+      Notifications.requestPermission();
+
     Notifications.createNotification('time', 'name');
+  };
 
   return (
     <Container>
