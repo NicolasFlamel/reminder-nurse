bc065b40ab83bf9379f9848cad8918c20ab75f02
Amauri <hamauri@aol.com>
2025-07-31 09:40:40 -0700

    Changed NotificationToggle to an Alert
    updated styling

--- client/src/components/NotificationToggle/NotificationToggle.tsx
+++ client/src/components/NotificationToggle/NotificationToggle.tsx
@@ -1,6 +1,8 @@
 import './styles.css';
-import { Alert, Button, ButtonProps } from 'react-bootstrap';
+import { useState } from 'react';
 import { useNotify } from 'hooks/useNotify';
+import { Alert, Button, ButtonProps } from 'react-bootstrap';
+import { Variant } from 'react-bootstrap/esm/types';
 
 interface NotificationToggleProps {
   checkPermission: () => void;
@@ -29,9 +31,24 @@ export const NotificationToggle = ({
 };
 
 const NotificationBtn = (props: ButtonProps) => {
+  const [show, setShow] = useState(true);
+  const variant: Variant = 'warning';
+
   return (
-    <Button className="notification-btn" variant="secondary" {...props}>
-      Request Notifications
-    </Button>
+    <Alert show={show} variant={variant}>
+      <Alert.Heading>Notification System</Alert.Heading>
+      <p>
+        Website does not currently have access to your system's notifications
+      </p>
+      <hr />
+      <section className="notification-actions">
+        <Button className="notification-btn" variant={variant} {...props}>
+          Request Notifications
+        </Button>
+        <Button variant={variant} onClick={() => setShow(false)}>
+          Close
+        </Button>
+      </section>
+    </Alert>
   );
 };
