a68a046e205e60660ed48352d69b0e4b840c963d
Amauri <hamauri@aol.com>
2025-08-07 12:47:17 -0700

    removed api controller
    not needed as cron jobs will be done with sockets

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import homeRoutes from './homeRoutes';
-import apiRoutes from './api';
 
 const router = Router();
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
 export default router;
