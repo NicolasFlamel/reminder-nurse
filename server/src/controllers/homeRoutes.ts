3602ac5b4c8635a3be484736a58d55859ba1016c
Amauri <hamauri@aol.com>
2026-03-03 14:59:49 -0800

    server changes

--- server/src/controllers/homeRoutes.ts
+++ server/src/controllers/homeRoutes.ts
@@ -1,13 +1,15 @@
 import path from 'path';
 import { Router } from 'express';
+import { ENV } from '../ENV';
 
 const router = Router();
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = ENV.NODE_ENV === 'production';
 
 router.get('/{*any}', (req, res, next) => {
   if (!isProduction && req.originalUrl.startsWith('/graphql')) next();
-  // TODO handle pathing
-  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+
+  const CLIENT_PATH = path.resolve(process.cwd(), '../client');
+  res.sendFile(path.join(CLIENT_PATH, './dist/index.html'));
 });
 
 export default router;
